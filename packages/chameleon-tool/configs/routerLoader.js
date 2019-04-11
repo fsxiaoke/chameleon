bf20dab026daba95834feaf6ca5566c606b8e307
zongh <zongh@fxiaoke.com>
2019-04-11 10:34:57 +0800

    .default bug

--- packages/chameleon-tool/configs/routerLoader.js
+++ packages/chameleon-tool/configs/routerLoader.js
@@ -29,7 +29,7 @@ module.exports = function(content) {
             {
               path: "${item.url}",
               name: "${item.name}",
-              component: require("$PROJECT/src${item.path}.cml")
+              component: require("$PROJECT/src${item.path}.cml").default
             },
             `
         })
