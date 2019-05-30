76895e1aa63f54cc8f82c0fdefd8623e53441dec
zongh <253164149@qq.com>
2019-05-30 00:50:10 -0700

    alter init project config file

--- packages/chameleon-templates/project/chameleon.config.js
+++ packages/chameleon-templates/project/chameleon.config.js
@@ -1,44 +1,33 @@
 
 // 设置静态资源的线上路径
-const publicPath = '//www.static.chameleon.com/cml';
+const publicPath = '//www.fxiaoke.com';
 // 设置api请求前缀
-const apiPrefix = 'https://api.chameleon.com';
+const apiPrefix = 'https://www.fxiaoke.com';
 
 cml.config.merge({
-  templateLang: 'cml',
+  templateLang: 'vue',
   templateType: 'html',
-  platforms: ['web', 'weex', 'wx', 'baidu', 'alipay'],
-  buildInfo: {
-    wxAppId: '123456'
-  },
-  wx: {
-    dev: {
-    },
-    build: {
-      apiPrefix
-    }
+  platforms: ['web', 'weex'],
+  staticPath: 'res',  //weex编译后图片等静态文件存放路径
+  check: {
+	enable: false, //接口类型检查，请设置为false即不检查。由于有缺陷，true对于开发不友好
   },
   web: {
     dev: {
-      analysis: false,
-      console: false
+      apiPrefix:"http://localhost:9090/web/"//http请求前缀
     },
     build: {
-      analysis: false,
       publicPath: `${publicPath}/web/`,
       apiPrefix
     }
   },
   weex: {
     dev: {
+	singlePage:false,//单页应用  
     },
     build: {
-      publicPath: `${publicPath}/weex/`,
-      apiPrefix
-    },
-    custom: {
-      publicPath: `${publicPath}/wx/`,
-      apiPrefix
+      publicPath: `bundle://`,//静态文件发布地址前缀
+      singlePage:false,//多页应用
     }
   }
 })
