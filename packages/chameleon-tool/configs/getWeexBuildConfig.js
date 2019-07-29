6f645e1ab355b24fc167427c41188c70d5a4b109
zongh <253164149@qq.com>
2019-07-29 00:06:18 -0700

    bug-fix single page build

--- packages/chameleon-tool/configs/getWeexBuildConfig.js
+++ packages/chameleon-tool/configs/getWeexBuildConfig.js
@@ -18,13 +18,45 @@ module.exports = function (options) {
   var outputPath = path.resolve(options.root, 'dist/weex');
 
   const weexCommonConfig = getWeexCommonConfig(options);
+  let singlePage = cml.config.get().weex[options.media].singlePage;
+
+  if (singlePage) {
+    var buildConfig = {
+      output: {
+        path: `${outputPath}`,
+        filename: `[name]${cml.config.get().weex.hash ? '_[hash:7]' : ''}.js`
+      },
+      plugins: [
+        new CleanWebpackPlugin(['./*'], {root: outputPath, verbose: false}),
+        new ZipPlugin({
+          filename: utils.getEntryName() + `.zip`
+        })
+      ]
+    }
+    buildConfig.entry = entry;
+    weexCommonConfig.module.rules.forEach((e, index) => {
+      if (index === 2) {
+        e.options = {
+          limit: false, // 不做limit的base64转换，需要添加?inline参数
+          publicPath: cml.config.get().weex.publicPath,
+          name: `${cml.config.get().staticPath}/[name]${cml.config.get().weex.hash ? '_[hash:7]' : ''}.[ext]`
+        }
+      } else if (index === 4) {
+        e.options = {
+          publicPath: cml.config.get().weex.publicPath,
+          name: `${cml.config.get().staticPath}/[name]${cml.config.get().weex.hash ? '_[hash:7]' : ''}.[ext]`
+        }
+      }
+    })
+    return merge(weexCommonConfig, buildConfig);
+  }
   const configArr = []
   Object.keys(entry).forEach(key => {
     let {routerConfig} = cml.utils.getRouterConfig();
     if (argv[3] === 'build' && (argv[4] === '-f' || argv[4] === '--file') && key !== argv[5]) {
       return;
     }
-    let bundleName = routerConfig.routes.find(e => e.name === key).bundleName || '';
+    let bundleName = (routerConfig.routes.find(e => e.name === key) || {}).bundleName || '';
     bundleName = bundleName.replace(/\//g, '+');
     var buildConfig = {
       output: {
